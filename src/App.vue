d47a3a231ce1987428158c8d19a5f9032794c46c
Paul Salamone <93609282+paulsalamone@users.noreply.github.com>
2023-03-03 09:25:42 +0100

    add dark mode and font selector

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="wrapper">
+  <div id="wrapper" class="choose-san-serif">
     <header>
       <img src="./assets/images/logo.svg" alt="dictionary logo" />
-      <StyleSelectors />
+      <StyleSelectors :html-el="htmlEl" @set-theme="handleTheme" @set-font="handleFont" />
     </header>
 
     <main>
@@ -23,7 +23,37 @@ import ErrorView from './components/ErrorView.vue'
 
 export default {
   name: 'app',
-  components: { SearchBox, StyleSelectors, DefinitionView, NoDefinitionsView, ErrorView }
+  components: { SearchBox, StyleSelectors, DefinitionView, NoDefinitionsView, ErrorView },
+  data() {
+    return {
+      htmlEl: null
+    }
+  },
+  mounted() {
+    this.htmlEl = document.getElementsByTagName('html')[0]
+    const themeProperty = localStorage.getItem('theme')
+    const fontProperty = localStorage.getItem('font')
+    console.log(themeProperty, fontProperty)
+
+    if (themeProperty) {
+      this.htmlEl.dataset.theme = themeProperty
+    }
+    if (fontProperty) {
+      this.htmlEl.dataset.font = fontProperty
+    }
+  },
+  methods: {
+    handleTheme(theme) {
+      console.log('handleTheme theme:', theme)
+      localStorage.setItem('theme', theme)
+      this.htmlEl.dataset.theme = theme
+    },
+    handleFont(font) {
+      console.log('handleFont font: ', font)
+      localStorage.setItem('font', font)
+      this.htmlEl.dataset.font = font
+    }
+  }
 }
 </script>
 
--- src/components/StyleSelectors.vue
+++ src/components/StyleSelectors.vue
@@ -1,20 +1,39 @@
 <template>
   <div id="style-selectors" class="san-serif">
     <!-- FONTS -->
-    FONTS
+    <h2>FONTS:</h2>
+    <p>
+      <button @click="toggleFont('sanSerif')">San Serif</button>
+      <button @click="toggleFont('serif')">Serif</button>
+      <button @click="toggleFont('mono')">mono</button>
+    </p>
     <!-- COLORS -->
-    COLORS
+    <h2>COLORS:</h2>
+    <p>
+      <button @click="toggleTheme('dark')">Dark</button>
+      <button @click="toggleTheme('light')">Light Mode</button>
+    </p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'style-selectors',
-  data() {
-    return {}
-  },
-  mounted() {},
-  methods: {}
+
+  methods: {
+    toggleTheme(theme) {
+      console.log('toggleTheme: ', theme)
+      //   this.htmlElData.dataset.theme = theme
+      this.$emit('setTheme', theme)
+      //   localStorage.setItem('theme', theme)
+    },
+    toggleFont(font) {
+      console.log('toggleFont: ', font)
+      //   this.htmlElData.dataset.font = font
+      //   localStorage.setItem('font', font)
+      this.$emit('setFont', font)
+    }
+  }
 }
 </script>
 
